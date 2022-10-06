33e497f5fdec7aa49213a156c0663be051222a0d
nikkimk <nikkimk06@gmail.com>
2022-10-06 17:52:08 -0400

    Slide 54

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './u-alerts.js';
 import './u-card.js';
 import './u-mark.js';
 /* TODO: SLIDE 54 */
+import './u-footer.js';
 import './u-navbar.js';
 import './u-search.js';
 import './u-social-icon.js';
